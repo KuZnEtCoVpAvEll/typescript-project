6587d8b4a0b936856ad4c2226459fdb7068b5f2b
KuZnEtCoVpAvEll <donikep76223003@gmail.com>
2025-09-29 21:37:09 +0300

    Initial TypeScript project setup

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const username = "Pavel";
+const age = 25;
+const isActive = true;
+console.log(`Користувач: ${username}, Вік: ${age}, Активний: ${isActive}`);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const username: string = "Pavel";
+const age: number = 25;
+const isActive: boolean = true;
+
+console.log(`Користувач: ${username}, Вік: ${age}, Активний: ${isActive}`);
